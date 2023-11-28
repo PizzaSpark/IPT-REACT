2072e2855a9451b57af15c3779bc75443616e2e0
PizzaSpark <seanpizzaspark@gmail.com>
2023-11-28 08:34:56 +0800

    added files

--- dashboard/src/pages/Sidebar.js
+++ dashboard/src/pages/Sidebar.js
@@ -1,23 +1,54 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import "./Sidebar.css"; 
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Sidebar.css";
+import { Home, Login, PersonAdd } from "@mui/icons-material";
 
 function Sidebar() {
-  return (
-    <div className='main'>
-      <div className="sidebar">
-        <Link to='/loginmat'>
-            <div className="sidebar-content">
-                <h4>
-                    <center>
-                        Login
-                    </center>
-                </h4>
+    return (
+        <div className="main">
+            <div className="sidebar">
+
+                <h2>
+                  Mayahee
+                </h2>
+                <Link to="/">
+                    <div className="sidebar-content">
+                        <Home style={{ marginRight: "10px" }} />
+                        <h4>
+                            <center>Home</center>
+                        </h4>
+                    </div>
+                </Link>
+
+                <Link to="/loginmat">
+                    <div className="sidebar-content">
+                        <Login style={{ marginRight: "10px" }} />
+                        <h4>
+                            <center>Login Material</center>
+                        </h4>
+                    </div>
+                </Link>
+
+                <Link to="/login">
+                    <div className="sidebar-content">
+                        <Login style={{ marginRight: "10px" }} />
+                        <h4>
+                            <center>Login</center>
+                        </h4>
+                    </div>
+                </Link>
+
+                <Link to="/register">
+                    <div className="sidebar-content">
+                        <PersonAdd style={{ marginRight: "10px" }} />
+                        <h4>
+                            <center>Register</center>
+                        </h4>
+                    </div>
+                </Link>
             </div>
-        </Link>
-      </div>
-    </div>
-  )
+        </div>
+    );
 }
 
-export default Sidebar
+export default Sidebar;
